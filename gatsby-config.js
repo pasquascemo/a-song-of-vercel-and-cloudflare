15bc17689c67623dfcb243e6ceffa22d555a75c6
domjtalbot <dom@dom.codes>
2020-05-02 15:31:52 +0100

    chore: rename references of zeit to vercel
    [fixes ch321]

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,14 @@ module.exports = {
     {
       options: {
         icon: 'src/images/gatsby-icon.png',
-        name: 'A Song of ZEIT and Cloudflare',
-        short_name: 'A Song of ZEIT and Cloudflare',
+        name: 'A Song of Vercel and Cloudflare',
+        short_name: 'A Song of Vercel and Cloudflare',
         start_url: '/',
       },
       resolve: `gatsby-plugin-manifest`,
     },
   ],
   siteMetadata: {
-    title: 'A Song of ZEIT and Cloudflare',
+    title: 'A Song of Vercel and Cloudflare',
   }
 };
