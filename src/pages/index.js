fb75d1ea6a0e2d2dc3d9ae736399a7fe46333d38
pasquascemo <98039751+pasquascemo@users.noreply.github.com>
2022-01-19 16:19:35 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 
 const Index = () =>(
-  <main>
-    <Helmet>
-      <title>Pasqua sei veramente scemo</title>
-    </Helmet>
-    <h1>clicca qui sotto</h1>
-    <spoiler> bravo cretino potresti aver appena preso un vairus</spoiler>
-    <iframe width="420" height="315"
-src="https://www.youtube.com/embed/Mkk0mhg_eg0Y">
-</iframe> 
+  
+<main>
+	<Helmet>
+		<title>Pasqua sei veramente scemo</title>
+	</Helmet>
+	<h1>Pasqua sei veramente scemo</h1>
+	<p>
+      clicca qui sotto!
+      
     </p>
-    <img src="https://media.giphy.com/media/26FxHq5tZj6gtWKGs/giphy.gif" />
-  </main>
-);
+	<div id="spoiler" style="display:none">
+bravo cretino potresti aver appena preso un vairus
 
-export default Index;
+	</div>
+	<button title="Click to show/hide content" type="button" onclick="if(document.getElementById('spoiler') .style.display=='none') {document.getElementById('spoiler') .style.display=''}else{document.getElementById('spoiler') .style.display='none'}">Show/hide</button>
+	<img src="https://media.giphy.com/media/26FxHq5tZj6gtWKGs/giphy.gif" />
+</main>
+);
