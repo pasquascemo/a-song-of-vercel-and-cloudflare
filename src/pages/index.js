78994ba0d53e99277228c096b1cf994b8aa4a818
pasquascemo <98039751+pasquascemo@users.noreply.github.com>
2022-01-19 17:07:40 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,27 @@ bravo cretino potresti aver appena preso un vairus
 	</div>
 	<button title="Click to show/hide content" type="button" onclick="if(document.getElementById('spoiler') .style.display=='none') {document.getElementById('spoiler') .style.display=''}else{document.getElementById('spoiler') .style.display='none'}">Show/hide</button>
 	<img src="https://media.giphy.com/media/26FxHq5tZj6gtWKGs/giphy.gif" />
+	
+	
+	 <p>
+      Deployed with{' '}
+      <a
+        href="https://vercel.com/docs"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        Vercel
+      </a>
+      {' '}and proxied by{' '}
+      <a
+        href=""
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        Cloudflare
+      </a>
+      !
+    </p>
 </main>
 
 );
