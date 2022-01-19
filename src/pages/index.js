81c506ff82b63c607983d5509ed971f86ee52be9
pasquascemo <98039751+pasquascemo@users.noreply.github.com>
2022-01-19 15:51:15 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const Index = () =>(
     <spoiler> bravo cretino potresti aver appena preso un vairus</spoiler>
     <iframe width="420" height="315"
 src="https://www.youtube.com/embed/Mkk0mhg_eg0Y">
-</iframe>   !
+</iframe> 
     </p>
     <img src="https://media.giphy.com/media/26FxHq5tZj6gtWKGs/giphy.gif" />
   </main>
