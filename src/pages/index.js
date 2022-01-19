5a9a9e3bc392ab5615654700d6f5458e5cd6f15d
pasquascemo <98039751+pasquascemo@users.noreply.github.com>
2022-01-19 15:46:09 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,27 +4,13 @@ import { Helmet } from 'react-helmet';
 const Index = () =>(
   <main>
     <Helmet>
-      <title>A Song of Vercel and Cloudflare</title>
+      <title>Pasqua sei veramente scemo</title>
     </Helmet>
-    <h1>A Song of Vercel and Cloudflare</h1>
-    <p>
-      Deployed with{' '}
-      <a
-        href="https://vercel.com/docs"
-        rel="noreferrer noopener"
-        target="_blank"
-      >
-        Vercel
-      </a>
-      {' '}and proxied by{' '}
-      <a
-        href=""
-        rel="noreferrer noopener"
-        target="_blank"
-      >
-        Cloudflare
-      </a>
-      !
+    <h1>clicca qui sotto</h1>
+    <spoiler> bravo cretino potresti aver appena preso un vairus</spoiler>
+    <iframe width="420" height="315"
+src="https://www.youtube.com/embed/Mkk0mhg_eg0Y">
+</iframe>   !
     </p>
     <img src="https://media.giphy.com/media/26FxHq5tZj6gtWKGs/giphy.gif" />
   </main>
